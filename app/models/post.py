dd3c862e058a6636663aac24cba190e52d21be6b
dudu-theman <dustanley07@gmail.com>
2025-12-17 22:32:02 -0800

    first commit

---
+++ app/models/post.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from core.database import Base
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+
+
