d39880ef716539d3e3479f1346005188e39fe776
dudu-theman <dustanley07@gmail.com>
2026-01-05 16:25:57 -0600

    feat: CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.auth import router as auth_router
 from api.post import router as post_router
@@ -8,6 +9,16 @@ from database import Base, engine
 from database.models import User, Post, Comment  # noqa: F401
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 app.include_router(auth_router)
 app.include_router(post_router)
 app.include_router(comment_router)
